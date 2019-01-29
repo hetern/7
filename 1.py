9a19779d4570972fd7e095fa050a0099ec4a64e6
hetern <hetern@163.com>
2019-01-29 16:04:58 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -56,10 +56,11 @@ print(f2([1,3,6,9,0,8]))
 
 #检查字典的每一个value的长度,如果大于2，那么仅保留前两个长度的内容，并将新内容返回给调用者。
 def f3(s):
-    for i in s.items():
-        if len(i[1]) > 2 :
-            s[i[0]]=i[1][0]+i[1][1]
-    return s
+       tem={}
+        for k, v in s.items():
+            if len(v) > 2 :
+                tem[k]=v[0:2]
+        return tem
 
 dic = {"k1": "v1v1", "k2": [11,22,33,44]}
 print(f3(dic))
